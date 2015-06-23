9bacbd6102b69072fe8e4dc12388d1ec97a0cbf6
venkatesh <venkatesh.uideveloper@gmail.com>
2015-06-23 16:51:49 +0530

    typehead

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 var app = angular.module("venky",["ngRoute", "ngAnimate", "ui.bootstrap"]);
 
-app.controller('TypeaheadCtrl', function(){})
+app.controller('TypeaheadCtrl', function($scope){
+	 $scope.selected = undefined;
+	 $scope.captains = ['Virat Kohil','Ms Dhoni', 'GanGuli', 'sachin Tendular','Kapil Dev','Ravi Sasthri','Azzarudin'];
+});
